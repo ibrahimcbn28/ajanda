d893e315488af968172b7e36bcba30de3d60f589
İbrahim Çoban <ibrahim.cbn2832@gmail.com>
2024-12-12 23:04:55 +0300

    ajanda programı

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace AgendaApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleme.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.lblSelectedDate = new System.Windows.Forms.Label();
+            this.dataGridViewEvents = new System.Windows.Forms.DataGridView();
+            this.btnAddEvent = new System.Windows.Forms.Button();
+            this.btnEditEvent = new System.Windows.Forms.Button();
+            this.btnDeleteEvent = new System.Windows.Forms.Button();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(10, 20);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // lblSelectedDate
+            // 
+            this.lblSelectedDate.AutoSize = true;
+            this.lblSelectedDate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblSelectedDate.Location = new System.Drawing.Point(250, 20);
+            this.lblSelectedDate.Name = "lblSelectedDate";
+            this.lblSelectedDate.Size = new System.Drawing.Size(177, 21);
+            this.lblSelectedDate.TabIndex = 1;
+            this.lblSelectedDate.Text = "Seçilen Tarih: [Bugün]";
+            // 
+            // dataGridViewEvents
+            // 
+            this.dataGridViewEvents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEvents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8});
+            this.dataGridViewEvents.Location = new System.Drawing.Point(254, 44);
+            this.dataGridViewEvents.Name = "dataGridViewEvents";
+            this.dataGridViewEvents.Size = new System.Drawing.Size(520, 300);
+            this.dataGridViewEvents.TabIndex = 2;
+            // 
+            // btnAddEvent
+            // 
+            this.btnAddEvent.Location = new System.Drawing.Point(250, 380);
+            this.btnAddEvent.Name = "btnAddEvent";
+            this.btnAddEvent.Size = new System.Drawing.Size(120, 40);
+            this.btnAddEvent.TabIndex = 3;
+            this.btnAddEvent.Text = "Etkinlik Ekle";
+            this.btnAddEvent.Click += new System.EventHandler(this.btnAddEvent_Click);
+            // 
+            // btnEditEvent
+            // 
+            this.btnEditEvent.Location = new System.Drawing.Point(380, 380);
+            this.btnEditEvent.Name = "btnEditEvent";
+            this.btnEditEvent.Size = new System.Drawing.Size(120, 40);
+            this.btnEditEvent.TabIndex = 4;
+            this.btnEditEvent.Text = "Etkinlik Düzenle";
+            this.btnEditEvent.Click += new System.EventHandler(this.btnEditEvent_Click);
+            // 
+            // btnDeleteEvent
+            // 
+            this.btnDeleteEvent.Location = new System.Drawing.Point(510, 380);
+            this.btnDeleteEvent.Name = "btnDeleteEvent";
+            this.btnDeleteEvent.Size = new System.Drawing.Size(120, 40);
+            this.btnDeleteEvent.TabIndex = 5;
+            this.btnDeleteEvent.Text = "Etkinlik Sil";
+            this.btnDeleteEvent.Click += new System.EventHandler(this.btnDeleteEvent_Click);
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.HeaderText = "Başlık";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.HeaderText = "Tarih";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.HeaderText = "Saat";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.HeaderText = "Açıklama";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.lblSelectedDate);
+            this.Controls.Add(this.dataGridViewEvents);
+            this.Controls.Add(this.btnAddEvent);
+            this.Controls.Add(this.btnEditEvent);
+            this.Controls.Add(this.btnDeleteEvent);
+            this.Name = "Form1";
+            this.Text = "Ajanda Uygulaması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MonthCalendar monthCalendar2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private ajanda.ajandaDataSet ajandaDataSet;
+        private ajanda.ajandaDataSet1 ajandaDataSet1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AgendaApp
+{
+    public partial class Form1 : Form
+    {
+        private MonthCalendar monthCalendar1;
+        private Label lblSelectedDate;
+        private DataGridView dataGridViewEvents;
+        private Button btnAddEvent;
+        private Button btnEditEvent;
+        private Button btnDeleteEvent;
+
+        public Form1()
+        {
+          
+        }
+
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new MonthCalendar();
+            this.lblSelectedDate = new Label();
+            this.dataGridViewEvents = new DataGridView();
+            this.btnAddEvent = new Button();
+            this.btnEditEvent = new Button();
+            this.btnDeleteEvent = new Button();
+
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(10, 20);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.DateChanged += new DateRangeEventHandler(this.monthCalendar1_DateChanged);
+
+            // 
+            // lblSelectedDate
+            // 
+            this.lblSelectedDate.AutoSize = true;
+            this.lblSelectedDate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblSelectedDate.Location = new System.Drawing.Point(250, 20);
+            this.lblSelectedDate.Name = "lblSelectedDate";
+            this.lblSelectedDate.Size = new System.Drawing.Size(150, 21);
+            this.lblSelectedDate.Text = "Seçilen Tarih: [Bugün]";
+
+            // 
+            // dataGridViewEvents
+            // 
+            this.dataGridViewEvents.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEvents.Location = new System.Drawing.Point(250, 60);
+            this.dataGridViewEvents.Name = "dataGridViewEvents";
+            this.dataGridViewEvents.Size = new System.Drawing.Size(520, 300);
+            this.dataGridViewEvents.Columns.Add("Title", "Başlık");
+            this.dataGridViewEvents.Columns.Add("Date", "Tarih");
+            this.dataGridViewEvents.Columns.Add("Time", "Saat");
+            this.dataGridViewEvents.Columns.Add("Description", "Açıklama");
+
+            // Kolonları sadece okunur hale getirelim
+            this.dataGridViewEvents.Columns[0].ReadOnly = true;
+            this.dataGridViewEvents.Columns[1].ReadOnly = true;
+            this.dataGridViewEvents.Columns[2].ReadOnly = true;
+            this.dataGridViewEvents.Columns[3].ReadOnly = true;
+
+            // 
+            // btnAddEvent
+            // 
+            this.btnAddEvent.Location = new System.Drawing.Point(250, 380);
+            this.btnAddEvent.Name = "btnAddEvent";
+            this.btnAddEvent.Size = new System.Drawing.Size(120, 40);
+            this.btnAddEvent.Text = "Etkinlik Ekle";
+            this.btnAddEvent.Click += new EventHandler(this.btnAddEvent_Click);
+
+            // 
+            // btnEditEvent
+            // 
+            this.btnEditEvent.Location = new System.Drawing.Point(380, 380);
+            this.btnEditEvent.Name = "btnEditEvent";
+            this.btnEditEvent.Size = new System.Drawing.Size(120, 40);
+            this.btnEditEvent.Text = "Etkinlik Düzenle";
+            this.btnEditEvent.Click += new EventHandler(this.btnEditEvent_Click);
+
+            // 
+            // btnDeleteEvent
+            // 
+            this.btnDeleteEvent.Location = new System.Drawing.Point(510, 380);
+            this.btnDeleteEvent.Name = "btnDeleteEvent";
+            this.btnDeleteEvent.Size = new System.Drawing.Size(120, 40);
+            this.btnDeleteEvent.Text = "Etkinlik Sil";
+            this.btnDeleteEvent.Click += new EventHandler(this.btnDeleteEvent_Click);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.lblSelectedDate);
+            this.Controls.Add(this.dataGridViewEvents);
+            this.Controls.Add(this.btnAddEvent);
+            this.Controls.Add(this.btnEditEvent);
+            this.Controls.Add(this.btnDeleteEvent);
+            this.Name = "Form1";
+            this.Text = "Ajanda Uygulaması";
+            this.Load += new EventHandler(this.Form1_Load);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblSelectedDate.Text = $"Seçilen Tarih: {monthCalendar1.SelectionStart.ToShortDateString()}";
+            LoadEvents();
+        }
+
+        private void LoadEvents()
+        {
+            dataGridViewEvents.Rows.Clear();
+            dataGridViewEvents.Rows.Add("Toplantı", "12/12/2024", "14:00", "Yıllık planlama toplantısı");
+            dataGridViewEvents.Rows.Add("Seminer", "15/12/2024", "09:00", "Yeni yazılım teknolojileri semineri");
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            lblSelectedDate.Text = $"Seçilen Tarih: {e.Start.ToShortDateString()}";
+        }
+
+        private void btnAddEvent_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Etkinlik ekleme ekranı açılacak.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnEditEvent_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Etkinlik düzenleme ekranı açılacak.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnDeleteEvent_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewEvents.SelectedRows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dataGridViewEvents.SelectedRows)
+                {
+                    dataGridViewEvents.Rows.RemoveAt(row.Index);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Silmek için bir etkinlik seçin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
